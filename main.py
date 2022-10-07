687e7f6ed280bd43d8f22adfda607f8b4425e001
Henry Prescott <henry.prescott@glassworksvfx.com>
2022-10-07 19:12:06 +0100

    changed methods, starting to get somewhere

--- main.py
+++ main.py
@@ -1,67 +1,89 @@
-# https://towardsdatascience.com/finding-a-grocery-delivery-slot-the-smart-way-f4f0800c4afe
-
-import requests
-from datetime import datetime, timedelta
+import re
 import os
 from dotenv import load_dotenv
+import ast
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 
 MY_ENV_VAR = os.getenv('MY_ENV_VAR')
-from twilio.rest import Client
 
 url = os.environ['MAIN_URL']
 
-payload = {'csrfmiddlewaretoken': 'gwHsJhpbwvGYwMpmQPSIBAv192dIuxgq3WpakrGroKgyhrGkT7IuOIAoc5H0iqXm',
-           'username': os.environ['USER'],
-           'password': os.environ['PASSWORD']}
+##############################################################################
+def book_class():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-s = requests.Session()
-r1 = s.post(os.environ['LOGIN_URL'], data=payload, verify=False)
+    # site
+    driver.get(os.environ['LOGIN_URL'])
 
-start_date = datetime.today().date().strftime('%Y-%m-%dT%H:%M:%S')
-end_date = (datetime.today().date() + timedelta(days=14)).strftime('%Y-%m-%dT%H:%M:%S')
+    username = driver.find_element(By.ID, "id_email")
+    username.send_keys(os.environ['USER'] + Keys.RETURN)
+    username = driver.find_element(By.NAME, "password")
+    username.send_keys(os.environ['PASSWORD'] + Keys.RETURN)
 
-count = 0
+    buttons = driver.find_elements(By.XPATH, '//button')
 
-json_data = {"requestorigin": "gi",
-             "data": {"customer-id": 5783385, "count": count, "next": start_date, "previous": end_date, "results": []}}
+    list_button = ""
 
-# Make POST request to API, sending required json data
-#r2 = s.post(url, json=json_data)
+    for button in buttons:
+        if re.search('list', button.accessible_name, re.IGNORECASE):
+            list_button = button
 
-#r2.status_code  # Inspect status code of response
+    list_button.click()
 
-# # Initialise empty dictionary for data
-# slot_data = {}
-#
-# # Loop through json response and record slot status for each time slot
-# for slot_day in r.json()['data']['slot_days']:
-#
-#     slot_date = slot_day['slot_date']
-#
-#     for slot in slot_day['slots']:
-#         slot_time = slot['slot_info']['start_time']
-#         slot_time = datetime.strptime(slot_time, '%Y-%m-%dT%H:%M:%SZ')
-#
-#         slot_status = slot['slot_info']['status']
-#
-#         slot_data[slot_time.strftime('%H:%M:%S %d-%m-%Y')] = slot_status
-#
-# # Filter for available slots
-# available_list = [f'\n{key} - {value}' for (key, value) in slot_data.items() if value != 'UNAVAILABLE']
-#
-# # If any available slots exist, send a text notification
-# if len(available_list) > 0:
-#     account_sid = os.environ['TWILIO_ACT_SID']
-#     auth_token = os.environ['TWILIO_AUTH_TOKEN']
-#     client = Client(account_sid, auth_token)
-#
-#     message_txt = f'\nDelivery Slot/s Found: \n{" ".join(available_list)}'
-#
-#     message = client.messages \
-#         .create(
-#         body=message_txt,
-#         from_=os.environ['TWILIO_NUMBER'],
-#         to=os.environ['MY_NUMBER']
-#     )
+    Scrolldown(driver)
+
+    print("done")
+
+    day_elements = driver.find_elements(By.CLASS_NAME, "single-day")
+
+    # Build regex check based on days specified
+    booking_schedule = list(ast.literal_eval(os.environ['BOOKING_SCHEDULE']))
+
+    for day in day_elements:
+        day_and_date = day.text.split('\n')[0]
+        if re.search('Top', day_and_date):
+            day_and_date = day.text.split('\n')[1]
+
+        for booking_event in booking_schedule:
+            class_name_and_time = booking_event[0]
+            days_to_book_regex_string = ""
+
+            for item in booking_event[1]:
+                days_to_book_regex_string += item + "|"
+            days_to_book_regex_string = days_to_book_regex_string.rstrip(days_to_book_regex_string[-1])
+
+            day_to_book_regex = re.compile(days_to_book_regex_string)
+
+            if day_to_book_regex.match(day_and_date):
+                # print(day_and_date)
+                events = day.find_elements(By.CLASS_NAME, "offering-container")
+                for event in events:
+                    if re.search(class_name_and_time, event.accessible_name, re.IGNORECASE):
+                        print("Looking to book: " + day_and_date + " : " + event.accessible_name)
+
+    driver.close()
+
+    return False
+
+
+def Scrolldown(driver):
+    y = 5000
+    for timer in range(0, 7):
+        driver.execute_script("window.scrollTo(0, " + str(y) + ")")
+        y += 5000
+        time.sleep(0.5)
+
+
+##############################################################################
+
+# Main function
+
+book_class()
