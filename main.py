d85c9e62b88adcf10dbc8d4973477222e6439c8d
Henry <prescotthenry@hotmail.com>
2022-10-23 10:04:55 +0100

    set up for fleet

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import ast
 import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
