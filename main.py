2f0c8a805948b69bd3e7f953087ffe586f5aa295
Henry <prescotthenry@hotmail.com>
2022-10-29 22:31:21 +0100

    working and multi-user

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import ast
 import time
+from datetime import datetime
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -21,87 +22,122 @@ url = os.environ['MAIN_URL']
 
 ##############################################################################
 def book_class():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    # site
-    driver.get(os.environ['LOGIN_URL'])
+    users = list(ast.literal_eval(os.environ['USERS']))
+    passwords = list(ast.literal_eval(os.environ['PASSWORDS']))
 
-    username = driver.find_element(By.ID, "id_email")
-    username.send_keys(os.environ['USER'] + Keys.RETURN)
-    username = driver.find_element(By.NAME, "password")
-    username.send_keys(os.environ['PASSWORD'] + Keys.RETURN)
+    for index in range(len(users)):
 
-    buttons = driver.find_elements(By.XPATH, '//button')
+        print("Booking for: " + users[index] + "\n")
 
-    list_button = ""
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    for button in buttons:
-        if re.search('list', button.accessible_name, re.IGNORECASE):
-            list_button = button
+        # site
+        driver.get(os.environ['LOGIN_URL'])
 
-    list_button.click()
+        username = driver.find_element(By.ID, "id_email")
+        username.send_keys(users[index] + Keys.RETURN)
+        username = driver.find_element(By.NAME, "password")
+        username.send_keys(passwords[index] + Keys.RETURN)
 
-    Scrolldown(driver)
+        buttons = driver.find_elements(By.XPATH, '//button')
 
-    day_elements = driver.find_elements(By.CLASS_NAME, "single-day")
+        list_button = ""
 
-    # Build regex check based on days specified
-    booking_schedule = list(ast.literal_eval(os.environ['BOOKING_SCHEDULE']))
+        for button in buttons:
+            if re.search('list', button.accessible_name, re.IGNORECASE):
+                list_button = button
 
-    print(booking_schedule)
+        list_button.click()
 
-    for day in day_elements:
-        # print("Title: " + day.accessible_name)
-        day_and_date = day.text.split('\n')[0]
-        if re.search('Top', day_and_date):
-            day_and_date = day.text.split('\n')[1]
+        Scrolldown(driver)
 
-        for booking_event in booking_schedule:
-            class_name_and_time = r"^.*?\b" + booking_event[0][0] + r"\b.*?\b" + booking_event[0][1] + r"\b.*?\b" + booking_event[0][2]
-            days_to_book_regex_string = ""
+        day_elements = driver.find_elements(By.CLASS_NAME, "single-day")
 
-            for item in booking_event[1]:
-                days_to_book_regex_string += item + "|"
-            days_to_book_regex_string = days_to_book_regex_string.rstrip(days_to_book_regex_string[-1])
+        # Build regex check based on days specified
+        booking_schedule = list(ast.literal_eval(os.environ['BOOKING_SCHEDULE']))
 
-            day_to_book_regex = re.compile(days_to_book_regex_string)
+        print(booking_schedule)
 
-            if day_to_book_regex.match(day_and_date):
-                # print("REGEX Search string: " + class_name_and_time)
-                events = day.find_elements(By.CLASS_NAME, "offering-container")
+        for day in day_elements:
+            # print("Title: " + day.accessible_name)
+            day_and_date = day.text.split('\n')[0]
 
-                print(day_and_date)
+            if re.search('Top', day_and_date):
+                day_and_date = day.text.split('\n')[1]
 
-                for event in events:
-                    event_name = event.accessible_name
-                    # print("\tEvent: " + event.accessible_name)
+            date_info = day_and_date.replace('(','')
+            date_info = date_info.replace(')','')
 
-                    if re.search(class_name_and_time, event_name, re.MULTILINE | re.IGNORECASE | re.UNICODE):
-                        # check to see if already booked
-                        already_booked_check = "registered|waitlisted"
+            day_of_week = date_info.split(' ')[0]
+            month = date_info.split(' ')[1]
+            date = date_info.split(' ')[2]
+            date = re.sub('\D', '', date)
 
-                        if re.search(already_booked_check, event_name, re.MULTILINE | re.IGNORECASE | re.UNICODE):
-                            print("-->\t\tAlready booked: " + str(day_and_date) + " : " + str(event_name) + "\n")
-                        else:
-                            print("-->\t\tLooking to book: " + str(day_and_date) + " : " + str(event_name) + "\n")
+            today = datetime.today().date()
+            test_month = float(datetime.strptime(month, '%b').strftime("%m"))
+            this_month = float(today.strftime("%m"))
 
-                            if event:
-                                event.click()
-                                dialog_window = driver.find_element(By.XPATH, "//div[@role='dialog']")
-                                if dialog_window:
-                                    try:
-                                        join_button = WebDriverWait(driver, 10).until(
-                                            EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Join')]"))
-                                        )
+            if test_month < this_month: # gone into next year
+                date = datetime.strptime(str(int(today.strftime("%Y"))+1) + ' ' + month + ' ' + date + ' ' + day_of_week + ' ' + today.strftime("%X"), '%Y %b %d %A %X').date()
+            else:
+                date = datetime.strptime(today.strftime("%Y") + ' ' + month + ' ' + date + ' ' + day_of_week + ' ' + today.strftime("%X"), '%Y %b %d %A %X').date()
 
-                                        if join_button:
-                                            # WebDriverWait(driver,1)
-                                            join_button.click()
+            delta_days = date - today
+            delta_days = int(delta_days.days)
+            if delta_days < 2:
+                print("Skipping day as within 2 days...")
+                continue
+            elif delta_days > 14:
+                print("Cannot book further than 2 weeks in advance, finishing...")
+                break
 
-                                    finally:
-                                        driver.close()
+            for booking_event in booking_schedule:
+                class_name_and_time = r"^.*?\b" + booking_event[0][0] + r"\b.*?\b" + booking_event[0][1] + r"\b.*?\b" + booking_event[0][2]
+                days_to_book_regex_string = ""
 
-    driver.close()
+                for item in booking_event[1]:
+                    days_to_book_regex_string += item + "|"
+                days_to_book_regex_string = days_to_book_regex_string.rstrip(days_to_book_regex_string[-1])
+
+                day_to_book_regex = re.compile(days_to_book_regex_string)
+
+                if day_to_book_regex.match(day_and_date):
+                    # print("REGEX Search string: " + class_name_and_time)
+                    events = day.find_elements(By.CLASS_NAME, "offering-container")
+
+                    print(day_and_date)
+
+                    for event in events:
+                        event_name = event.accessible_name
+                        # print("\tEvent: " + event.accessible_name)
+
+                        if re.search(class_name_and_time, event_name, re.MULTILINE | re.IGNORECASE | re.UNICODE):
+                            # check to see if already booked
+                            already_booked_check = "registered|waitlisted|waitlist"
+
+                            if re.search(already_booked_check, event_name, re.MULTILINE | re.IGNORECASE | re.UNICODE):
+                                print("-->\t\tAlready booked: " + str(day_and_date) + " : " + str(event_name) + "\n")
+                            else:
+                                print("-->\t\tLooking to book: " + str(day_and_date) + " : " + str(event_name) + "\n")
+
+                                if event:
+                                    event.click()
+                                    dialog_window = driver.find_element(By.XPATH, "//div[@role='dialog']")
+                                    if dialog_window:
+                                        try:
+                                            join_button = WebDriverWait(driver, 10).until(
+                                                EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Join') or contains(., 'Book')]"))
+                                            )
+
+                                            if join_button:
+                                                # WebDriverWait(driver,1)
+                                                join_button.click()
+
+                                        finally:
+                                            driver.close()
+
+        driver.close()
 
     return False
 
